e717340b00b6405949d94d5f79173ddbb41411b8
Aviij <avishkadd9@gmail.com>
2024-02-16 12:21:07 +0530

    Project Completed!

--- script.js
+++ script.js
@@ -13,22 +13,33 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+//addOrUpdateTasks 
 const addOrUpdateTask = () => {
+
+  addOrUpdateTaskBtn.innerText = "Add Task";
+
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
   const taskObj = {
+
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     title: titleInput.value,
     date: dateInput.value,
     description: descriptionInput.value,
+
   };
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+  } else {
+    taskData[dataArrIndex] = taskObj;
   }
 
+  updateTaskContainer();
+  reset();
 
-  updateTaskContainer()
-  reset()
+  localStorage.setItem("data",JSON.stringify(taskData));
+
 };
 
 const updateTaskContainer = () => {
@@ -89,7 +100,9 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-  if (formInputsContainValues) {
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+  if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
   } else {
     reset();
